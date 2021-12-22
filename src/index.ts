5cf6901c60d34ee1c7990c263be12327ef9e94a2
Stephen Alvin <stephenalvin0912@gmail.com>
2021-12-22 13:52:59 +0800

    Put back OpenAPI and Cors

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import {EzBackend, EzModel, Type} from '@ezbackend/common'
 import { EzOpenAPI } from "@ezbackend/openapi";
 import { EzDbUI } from "@ezbackend/db-ui";
 import { EzCors } from "@ezbackend/cors";
-
 const app = new EzBackend()
-
+//---Plugins---
+//Everything is an ezapp in ezbackend
+app.addApp(new EzOpenAPI())
+app.addApp(new EzCors())
+//---Plugins---
 const todo = new EzModel('Todo', {
   content: Type.VARCHAR,
 })
