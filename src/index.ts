9908f65fea2cbf4415d928489c29acb2398548f2
Stephen Alvin <stephenalvin0912@gmail.com>
2021-12-22 12:56:54 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import {EzBackend, EzModel, Type} from '@ezbackend/common'
+import { EzOpenAPI } from "@ezbackend/openapi";
+import { EzDbUI } from "@ezbackend/db-ui";
+import { EzCors } from "@ezbackend/cors";
+
+const app = new EzBackend()
+
+//---Plugins---
+//Everything is an ezapp in ezbackend
+app.addApp(new EzOpenAPI())
+app.addApp(new EzDbUI())
+app.addApp(new EzCors())
+//---Plugins---
+
+const todo = new EzModel('Todo', {
+  content: Type.VARCHAR,
+})
+
+app.addApp(todo, { prefix: 'todo' })
+
+app.start()
